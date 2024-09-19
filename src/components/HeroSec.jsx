4c5378e5207019f6ffa272555b66aede300802e6
5huv0 <md88748874@gmail.com>
2024-09-19 20:42:03 +0600

    hero section doen

--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -8,10 +8,21 @@ export default function HeroSec(){
             <p className="main-div-p">"Photography is more than just capturing images; it's about preserving fleeting moments that might otherwise be lost to time. Each photograph tells a unique story, a glimpse into the past that holds emotions, memories, and experiences within its frame. It has the power to evoke feelings and transport us back to the exact moment the picture was taken. Through photography, we see the world in new ways, finding beauty in the ordinary and the extraordinary alike. It allows us to communicate without words, capturing the essence of life and the world around us, one shutter click at a time."</p>
             <button className="main-btn">Get started</button>
             <div className="main-linkings">
-                <img src="./images/linkedin-logo.png" alt="" className="img-link"/>
-                <img src="./images/facebook-logo.png" alt="" className="img-link"/>
-                <img src="./images/twitter-logo.png" alt="" className="img-link"/>
-                <img src="./images/instagram-logo.png" alt="" className="img-link"/>
+                <a href="">
+                    <img src="./images/linkedin-logo.png" alt="" className="img-link"/>
+                </a>
+
+                <a href="">
+                    <img src="./images/facebook-logo.png" alt="" className="img-link"/>
+                </a>
+
+                <a href="">
+                    <img src="./images/twitter-logo.png" alt="" className="img-link"/>
+                </a>
+
+                <a href="">
+                    <img src="./images/instagram-logo.png" alt="" className="img-link"/ >
+                </a>
             </div>
         </div>
     )
