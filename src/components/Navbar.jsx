9d2abafa809953907f2b15b8a2a683859abab71d
5huv0 <md88748874@gmail.com>
2024-09-19 10:56:42 +0600

    navbar almost done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,36 +2,45 @@ import React from "react";
 
 export default function Navbar(){
     return(
-        <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar w/ text</a>
-            <button 
-                className="navbar-toggler" 
-                type="button" data-bs-toggle="collapse" 
-                data-bs-target="#navbarText" 
-                aria-controls="navbarText" 
-                aria-expanded="false" 
-                aria-label="Toggle navigation"
-            >
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarText">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Features</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Pricing</a>
-                    </li>
-                </ul>
-                <span className="navbar-text">
-                    Navbar text with an
-                </span>
+        <nav className="navbar navbar-expand-lg fixed-top">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <img src="./images/logo-nav.png" alt="Navbar logo" className="nav-logo"/>
+                </a>
+                <img src="./images/logo-line.png" alt="" className="logo-line"/>
+                <button 
+                    className="navbar-toggler" 
+                    type="button" data-bs-toggle="collapse" 
+                    data-bs-target="#navbarText" 
+                    aria-controls="navbarText" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarText">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Portfolio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Pages</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                    <span className="navbar-text">
+                        Let's Work
+                    </span>
+                </div>
             </div>
-        </div>
         </nav>
     )
 }
