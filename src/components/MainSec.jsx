b7016eb28c7f403b14cd98aebac5b89b3007402b
5huv0 <md88748874@gmail.com>
2024-09-20 00:06:35 +0600

    my service  section starting

--- src/components/MainSec.jsx
+++ src/components/MainSec.jsx
@@ -74,6 +74,30 @@ export default function MainSec(){
 
 
             <div className="my-service-sec">
+                <div className="text-center">
+                    <div className="row">
+                        <div className="col">
+                        1 of 3
+                        </div>
+                        <div className="col">
+                        2 of 3 (wider)
+                        </div>
+                        <div className="col">
+                        3 of 3
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                        1 of 3
+                        </div>
+                        <div className="col">
+                        2 of 3 (wider)
+                        </div>
+                        <div className="col">
+                        3 of 3
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
