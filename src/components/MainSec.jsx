22ee3c1b6036bb50784dd1110825d197adae2388
5huv0 <md88748874@gmail.com>
2024-09-19 21:16:34 +0600

    working with main section

---
+++ src/components/MainSec.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function MainSec(){
+    return(
+        <div>
+           <div className="achievements-sec">
+            <div className="text-center">
+                <div className="row">
+                    <div className="col">
+                        <p className="achi-head">Experience</p>
+                        <p className="achi-des">7+ years of experiences</p>
+                    </div>
+
+                    <div className="col">
+                        <p className="achi-head">Services</p>
+                        <p className="achi-des">Photography, Videographpy, Editing</p>
+                    </div>
+
+                    <div className="col">
+                        <p className="achi-head">Partners</p>
+                        <p className="achi-des">Lorem, Ipsum, Loremm</p>
+                    </div>
+
+                    <div className="col">
+                        <p className="achi-head">Achievements</p>
+                        <p className="achi-des">
+                            2019 Best photographer,
+                            2018 Youngest photographer of CPS &
+                            2024 Runners up of CPS photo award
+                        </p>
+                    </div>
+                </div>
+            </div>
+           </div>
+        </div>
+    )
+}
