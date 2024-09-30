c6e74e04bda9cc03f7886609bbc0468c14ce1440
5huv0 <md88748874@gmail.com>
2024-09-30 12:20:29 +0600

    working on progress on event section

--- src/components/EventsSec.jsx
+++ src/components/EventsSec.jsx
@@ -5,15 +5,26 @@ export default function EventSec(){
 
     return(
         <div>
-            <div>
+            <div className="main-event-sec">
                 <div class="container text-start">
                     <div class="row">
                         <div class="col">
-                            <p></p>
-                            <p></p>
-                            <p></p>
-                            <p></p>
-                            <button></button>
+                            <p className="event-main-head">
+                                Creative Portrait 
+                                <img src="./images/vertical-line.png" alt="" />
+                            </p>
+                            <span>Client</span><p className="event-main-name">
+
+                            </p>
+                            <p className="event-main-category">
+
+                            </p>
+                            <p className="event-main-des">
+
+                            </p>
+                            <button>
+
+                            </button>
                         </div>
                         <div class="col">
                             <img src="" alt="" />
@@ -26,14 +37,25 @@ export default function EventSec(){
                 <div class="container text-start">
                     <div class="row">
                         <div class="col">
-                            <p></p>
-                            <p></p>
-                            <p></p>
-                            <p></p>
-                            <button></button>
+                            <img src="" alt="" />
                         </div>
                         <div class="col">
-                            <img src="" alt="" />
+                            <p className="event-main-head">
+                                The Imanination
+                                <img src="./images/vertical-line.png" alt="" />
+                            </p>
+                            <span>Client</span><p className="event-main-name">
+
+                            </p>
+                            <p className="event-main-category">
+
+                            </p>
+                            <p className="event-main-des">
+
+                            </p>
+                            <button>
+
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -43,11 +65,22 @@ export default function EventSec(){
                 <div class="container text-start">
                     <div class="row">
                         <div class="col">
-                            <p></p>
-                            <p></p>
-                            <p></p>
-                            <p></p>
-                            <button></button>
+                            <p className="event-main-head">
+                                Happy Wedding
+                                <img src="./images/vertical-line.png" alt="" />
+                            </p>
+                            <span>Client</span><p className="event-main-name">
+
+                            </p>
+                            <p className="event-main-category">
+
+                            </p>
+                            <p className="event-main-des">
+
+                            </p>
+                            <button>
+
+                            </button>
                         </div>
                         <div class="col">
                             <img src="" alt="" />
