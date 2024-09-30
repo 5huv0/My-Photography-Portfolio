a0b27cdaebdc00e0cd6da3511434375f7bbbf467
5huv0 <md88748874@gmail.com>
2024-09-30 14:01:18 +0600

    client section almost done

--- src/components/EventsSec.jsx
+++ src/components/EventsSec.jsx
@@ -6,52 +6,56 @@ export default function EventSec(){
     return(
         <div>
             <div className="main-event-sec">
-                <div class="container text-start">
-                    <div class="row">
-                        <div class="col">
+                <div className="container text-start">
+                    <div className="row">
+                        <div className="col">
                             <p className="event-main-head">
                                 Creative Portrait 
                                 <img src="./images/vertical-line.png" alt="" />
                             </p>
-                            <span>Client</span><p className="event-main-name">
-
+                            
+                            <p className="event-main-name">
+                                <span>Client : </span>Sadman Shoumick
                             </p>
+                            
                             <p className="event-main-category">
-
+                                <span>Category : </span>
                             </p>
                             <p className="event-main-des">
-
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum commodi in placeat aperiam repellendus mollitia autem officiis sapiente excepturi odit! Nam voluptate deleniti aliquam assumenda voluptatem, numquam a excepturi incidunt.
                             </p>
                             <button>
 
                             </button>
                         </div>
-                        <div class="col">
-                            <img src="" alt="" />
+                        <div className="col">
+                            <img src="./images/shoumick.jpg" alt="" className="event-main-img"/>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div>
-                <div class="container text-start">
-                    <div class="row">
-                        <div class="col">
-                            <img src="" alt="" />
+            <div className="main-event-sec">
+                <div className="container text-start">
+                    <div className="row">
+                        <div className="col">
+                            <img src="./images/shihab.jpg" alt="" className="event-main-img"/>
                         </div>
-                        <div class="col">
+                        <div className="col">
                             <p className="event-main-head">
                                 The Imanination
                                 <img src="./images/vertical-line.png" alt="" />
                             </p>
-                            <span>Client</span><p className="event-main-name">
-
+                            
+                            <p className="event-main-name">
+                                <span>Client : </span>Shihab Sarar
                             </p>
+                            
                             <p className="event-main-category">
-
+                                <span>Category : </span>
                             </p>
                             <p className="event-main-des">
-
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, placeat voluptate? Atque exercitationem aliquid ipsum perferendis est architecto impedit porro rerum mollitia numquam. Dicta neque dolor eos fugit asperiores nam?
                             </p>
                             <button>
 
@@ -61,29 +65,30 @@ export default function EventSec(){
                 </div>
             </div>
 
-            <div>
-                <div class="container text-start">
-                    <div class="row">
-                        <div class="col">
+            <div className="main-event-sec">
+                <div className="container text-start">
+                    <div className="row">
+                        <div className="col">
                             <p className="event-main-head">
                                 Happy Wedding
                                 <img src="./images/vertical-line.png" alt="" />
                             </p>
-                            <span>Client</span><p className="event-main-name">
-
+                            <p className="event-main-name">
+                               <span>Client : </span> Raisul Karim
                             </p>
+                            
                             <p className="event-main-category">
-
+                                <span>Category : </span>
                             </p>
                             <p className="event-main-des">
-
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi odio culpa exercitationem laborum veritatis rem accusamus ullam, voluptates, eius maiores harum illum incidunt praesentium odit, dolorum nihil. Autem, unde maxime.
                             </p>
                             <button>
 
                             </button>
                         </div>
-                        <div class="col">
-                            <img src="" alt="" />
+                        <div className="col">
+                            <img src="./images/saju.jpg" alt="" className="event-main-img"/>
                         </div>
                     </div>
                 </div>
