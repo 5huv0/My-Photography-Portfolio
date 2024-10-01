94b79a4b5391d69bbb5f7051bfbe8906b2f4f178
5huv0 <md88748874@gmail.com>
2024-10-02 00:58:39 +0600

    ok

--- src/components/ClientSec.jsx
+++ src/components/ClientSec.jsx
@@ -8,6 +8,7 @@ export default function Client(props){
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
+                
                 <div className="card-body">
                     <a href="#" className="card-link">Card link</a>
                     <a href="#" className="card-link">Another link</a>
