b1be44eef913f10745a5465d756b33fde8135bda
5huv0 <md88748874@gmail.com>
2024-10-03 12:26:03 +0600

    footer  section one done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,19 +10,19 @@ export default function Footer(){
                     <h2>Get In Touch</h2>
                     <div className="row">
                         <div className="col">
-                            <img src="./images/footer-add.png" alt="" />
+                            <img src="./images/footer-add.png" alt="" className="footer-img"/>
                             <p>Address</p>
                             <p>Sylhet Engineering College, Sylhet</p>
                         </div>
                         <div className="col">
-                            <img src="./images/footer-email.png" alt="" />
+                            <img src="./images/footer-email.png" alt="" className="footer-img"/>
                             <p>Email</p>
                             <p>shuvo88745@gmail.com</p>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col">
-                            <img src="./images/footer-phone.png" alt="" />
+                            <img src="./images/footer-phone.png" alt="" className="footer-img"/>
                             <p>Phone</p>
                             <p>+8802357623</p>
                         </div>
