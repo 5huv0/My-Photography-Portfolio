b72f9099f62ca76487783da18c66c42389b19bd3
5huv0 <md88748874@gmail.com>
2024-10-04 10:28:24 +0600

    doing the footer last section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,21 +9,21 @@ export default function Footer(){
                     <p>Contacts</p>
                     <h2>Get In Touch</h2>
                     <div className="row">
-                        <div className="col">
+                        <div className="col footer-linkings">
                             <img src="./images/footer-add.png" alt="" className="footer-img"/>
-                            <p>Address</p>
+                            <h3>Address </h3>
                             <p>Sylhet Engineering College, Sylhet</p>
                         </div>
-                        <div className="col">
+                        <div className="col footer-linkings">
                             <img src="./images/footer-email.png" alt="" className="footer-img"/>
-                            <p>Email</p>
+                            <h3>Email</h3>
                             <p>shuvo88745@gmail.com</p>
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col">
+                        <div className="col footer-linkings">
                             <img src="./images/footer-phone.png" alt="" className="footer-img"/>
-                            <p>Phone</p>
+                            <h3>Phone</h3>
                             <p>+8802357623</p>
                         </div>
                         
@@ -48,9 +48,60 @@ export default function Footer(){
                             </button>
                         </div>
                     </div>
-
                 </div>
             </footer>
+            <div className="footer-last-sec">
+                    <div>
+                        <img src="./images/logo-nav.png" alt="" />
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore sunt molestias earum cupiditate quibusdam, aperiam laboriosam voluptatum impedit ullam sapiente deleniti non. Dolorem, eaque temporibus autem perferendis ipsam sapiente quo?
+                        </p>
+                    </div>
+                    <div> 
+                        <h3>
+                            Quicklinks
+                        </h3>
+                        <a href="">
+                            Home
+                        </a>
+                        <a href="">
+                            About
+                        </a>
+                        <a href="">
+                            FAQs
+                        </a>
+                        <a href="">
+                            Blog
+                        </a>
+                        <a href="">
+                            Contact
+                        </a>
+                    </div>
+
+                    <div>
+                        <h3>
+                            Get In Touch
+                        </h3>
+                        <p>
+                            A: Sylhet
+                        </p>
+                        <p>
+                            P: +8802357623
+                        </p>
+                        <p>
+                            E: shuvo88745@gmail.com
+                        </p>
+                        <a href="">
+                            <img src="./images/facebook-logo.png" alt="" />
+                        </a>
+                        <a href="">
+                            <img src="./images/instagram-logo.png" alt="" />
+                        </a>
+                        <a href="">
+                            <img src="./images/linkedin-logo.png" alt="" />
+                        </a>
+                    </div>
+                </div>
         </div>
     )
 }
